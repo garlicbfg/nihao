540abaa86ae6ce6bbebf4064e4d2167a54d62a64
garlicbfg <141194396+garlicbfg@users.noreply.github.com>
2023-09-09 02:26:24 +0800

    minor changes

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # nihao
 
-# how to deal with tokens, use command.
-# git remote set-url origin https://<USERNAME>:<MYTOKEN>@github.com/<USERNAME>/<REPO>.git
+# how to deal with tokens: use command "git remote set-url origin https://<USERNAME>:<MYTOKEN>@github.com/<USERNAME>/<REPO>.git"
+#
+#
---
+++ Unitool/src/main.rs
@@ -0,0 +1,46 @@
+use std::io::{self, Write};
+
+mod instruction_parser;
+
+
+
+
+fn main() 
+{
+    let mut parser = instruction_parser::InstructionParser::new();
+
+    parser.add("exit");
+    parser.add("add i s r -ba");
+    parser.add("add i s -ba");
+    parser.add("run s -x");
+    parser.add("run -x");
+    println!("Printing possible instructions:");
+    println!("{:?}", parser);
+
+    println!("Enter instruction:");
+    loop
+    {
+        let mut input_str = String::new();
+
+        print!(":");
+        io::stdout().flush();
+        io::stdin()
+            .read_line(&mut input_str)
+            .expect("Failed to read line");
+    
+            //let guess: u32 = match input_str.trim().parse()
+            //{
+                //Ok(num) => num,
+                //Err(_) => continue,
+            //};
+        
+            let v: Vec<&str> = input_str.split(' ').collect();
+
+            for s in v 
+            {
+                println!("{}", s)
+            }
+
+            //println!("You entered: {}", input_str);
+    }
+}
