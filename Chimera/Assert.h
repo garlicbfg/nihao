dc3a0705efaf313ea35b8b8e26068377f4fab975
garlicbfg <141194396+garlicbfg@users.noreply.github.com>
2024-02-09 15:26:41 +0800

    minor changes

--- Chimera/Assert.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Windows.h"
-#include <stdio.h>
-
-extern void debugAssert(bool expressionResult, const char* expression, const char* module, int line);
-
-#define DBG(expr) debugAssert(!!(expr), #expr, __FUNCTION__, __LINE__)
-#define ASSERT DBG
